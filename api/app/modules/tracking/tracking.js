c9f82e871136242a07dab6edc7e832cc58761f3f
James <jknight@lunenfeld.ca>
2019-01-09 13:03:16 -0500

    Final changes for publication
    - remove IP location retrieval
    - update production scripts to use node 10.13

--- api/app/modules/tracking/tracking.js
+++ api/app/modules/tracking/tracking.js
@@ -1,11 +1,8 @@
 const insert = require('../db-methods/insert');
-const ipInfo = require('./ip-info');
 
 const tracking = (ip, species, field, gene) => (
   new Promise((resolve) => {
-    const info = ipInfo(ip);
     const insertObj = {
-      country: info.country,
       field,
       gene,
       species,
