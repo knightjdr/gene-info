d5db30124a81fb32a3f5c031f6488c0295014848
James <jknight@lunenfeld.ca>
2019-01-11 16:04:22 -0500

    Fix search bar reports opening on every tab

--- chrome-extension/src/popup/listeners/on-enter.js
+++ chrome-extension/src/popup/listeners/on-enter.js
@@ -1,10 +1,10 @@
-import updateTab from './update-tab';
+import { searchTab } from './update-tab';
 
 const onEnter = function enter(e) {
   if (e.keyCode === 13) {
     const { value } = this;
     if (value) {
-      updateTab('search', value);
+      searchTab('search', value);
     }
   }
 };
--- chrome-extension/src/popup/listeners/update-tab.js
+++ chrome-extension/src/popup/listeners/update-tab.js
@@ -1,3 +1,9 @@
+export const searchTab = (action, setting, value) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action, setting, value });
+  });
+};
+
 // Send object to content script. Do this for all tabs as they should all update
 // When a setting changes.
 const updateTab = (action, setting, value) => {
