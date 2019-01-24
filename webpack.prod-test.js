99cc1a81c1b383232af171ee9044e4a34437803e
James <jknight@lunenfeld.ca>
2019-01-24 12:10:43 -0500

    Create test build
    - remove about link
    - remove tabs permission

---
+++ webpack.prod-test.js
@@ -0,0 +1,25 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'chrome-extension/dist-test'),
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'chrome-extension/dev-build/manifest.json', to: 'manifest.json' },
+      { from: 'chrome-extension/dev-build/popup.html', to: 'popup.html' },
+      { from: 'chrome-extension/dev-build/icon-test', to: 'icon/' },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      path: path.resolve(__dirname, 'chrome-extension/dist-test'),
+    }),
+  ],
+});
