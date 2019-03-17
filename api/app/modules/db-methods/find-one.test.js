c38bdc8ac9eaf4b0e630c6564a15c344b1439702
James Knight <knightjdr@gmail.com>
2019-03-17 12:41:17 -0400

    Use mongo memory database for db query methods

--- api/app/modules/db-methods/find-one.test.js
+++ api/app/modules/db-methods/find-one.test.js
@@ -1,56 +1,69 @@
-process.env.NODE_ENV = test;
-const { ObjectID } = require('mongodb');
+const mongodb = require('mongo-mock');
 
 const database = require('../../connections/database');
+const findOne = require('./find-one');
 
-// mock config
 jest.mock('../../../config', () => (
   {
     database: {
-      name: 'sandbox',
       prefix: 'test_',
-      pw: 'sandboxpw',
-      user: 'sandbox',
     },
   }
 ));
-// mock logger
+jest.mock('../../connections/database', () => ({
+  connection: null,
+}));
 jest.mock('../../../logger');
 
-const findOne = require('./find-one');
-
-// expected return values
-const response = {
-  missing: [],
-  one: [{ _id: ObjectID('5aa6ac91c63eb43ab21a072c'), name: 'test', field: 'a' }],
-  subset: [{ field: 'a' }],
-};
-
-beforeAll(() => (
-  database.init()
-));
-afterAll(() => (
-  database.close()
-));
+beforeAll(async (done) => {
+  mongodb.max_delay = 0;
+  const { MongoClient } = mongodb;
+  const url = 'mongodb://localhost:27017/test';
+  // Use connect method to connect to the Server
+  MongoClient.connect(url, {}, (err, db) => {
+    database.connection = db;
+    // Insert some documents
+    const docs = [
+      { _id: 1, name: 'test' },
+    ];
+    db.collection('test_documents').insertMany(docs, () => {
+      done();
+    });
+  });
+});
 
 describe('Query for a single entry', () => {
   describe('when successful', () => {
     it('should find one record in the database', () => (
-      findOne('get', { name: 'test' }).then((getCollection) => {
-        expect(getCollection).toEqual(response.one);
+      findOne('documents', { name: 'test' }).then((getCollection) => {
+        const expected = [{ _id: 1, name: 'test' }];
+        expect(getCollection).toEqual(expected);
       })
     ));
 
     it('should subset returned documents from database', () => (
-      findOne('get', {}, { _id: 0, field: 1 }).then((getCollection) => {
-        expect(getCollection).toEqual(response.subset);
+      findOne('documents', {}, { _id: 0, name: 1 }).then((getCollection) => {
+        const expected = [{ name: 'test' }];
+        expect(getCollection).toEqual(expected);
       })
     ));
 
     it('should not find a record in the database', () => (
-      findOne('get', { name: 'test-missing' }).then((getCollection) => {
-        expect(getCollection).toEqual(response.missing);
+      findOne('documents', { name: 'test-missing' }).then((getCollection) => {
+        const expected = [];
+        expect(getCollection).toEqual(expected);
       })
     ));
   });
+});
+
+describe('Query throwing an error', () => {
+  beforeAll(() => {
+    // Make database undefined to cause an error to throw.
+    database.connection = undefined;
+  });
+
+  it('should throw an error', () => (
+    expect(findOne('documents', { name: 'test' })).rejects.not.toBeNull()
+  ));
 });
