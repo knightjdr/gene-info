185670acc3ef93e0daf32dc7b16c742966f6a0e8
James <jknight@lunenfeld.ca>
2019-03-20 10:21:12 -0400

    Add theme to no result tooltip

--- chrome-extension/src/content/templates/tooltip/no-result.js
+++ chrome-extension/src/content/templates/tooltip/no-result.js
@@ -1,6 +1,11 @@
+import State from '../../state';
+
 const noResult = () => (`
   <aside id="backdrop">
-    <div id="tooltip">
+    <div
+      class="theme_${State.settings.theme}"
+      id="tooltip"
+    >
       <header>No gene information available</header>
     </div>
   </aside>
