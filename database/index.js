ff7d2be9938d9c90ee2781caa521c9ae4f4d1491
James Knight <knightjdr@gmail.com>
2021-05-15 10:59:59 -0400

    Update Compartment DB url

--- database/index.js
+++ database/index.js
@@ -19,7 +19,7 @@ const options = args();
 /* Retrieve and perform initial processing of database files, including
 ** unzip and splitting to species files as needed. DB files are split to
 ** individual species files as they are generally too large to read into
-** memort otherwise. Pfam retrieval and processing is done after Uniprot
+** memory otherwise. Pfam retrieval and processing is done after Uniprot
 ** as splitting domains to individual files requires a list of Uniprot IDs to
 ** keep. */
 
--- database/localization/localization.js
+++ database/localization/localization.js
@@ -1,7 +1,6 @@
 /* eslint max-len: 0 */
 
 const createFolder = require('../helpers/create-folder');
-const downloadHttp = require('../helpers/download-http');
 const downloadHttps = require('../helpers/download-https');
 const unzipFile = require('../helpers/unzip-file');
 
@@ -14,27 +13,27 @@ const fsConfig = {
   hpaFolder: './files/localization/hpa',
   compartments: [
     {
-      url: 'http://download.jensenlab.org/arabidopsis_compartment_integrated_full.tsv',
+      url: 'https://download.jensenlab.org/arabidopsis_compartment_integrated_full.tsv',
       file: './files/localization/compartments/Arabidopsis thaliana.tsv',
     },
     {
-      url: 'http://download.jensenlab.org/worm_compartment_integrated_full.tsv',
+      url: 'https://download.jensenlab.org/worm_compartment_integrated_full.tsv',
       file: './files/localization/compartments/Caenorhabditis elegans.tsv',
     },
     {
-      url: 'http://download.jensenlab.org/fly_compartment_integrated_full.tsv',
+      url: 'https://download.jensenlab.org/fly_compartment_integrated_full.tsv',
       file: './files/localization/compartments/Drosophila melanogaster.tsv',
     },
     {
-      url: 'http://download.jensenlab.org/human_compartment_integrated_full.tsv',
+      url: 'https://download.jensenlab.org/human_compartment_integrated_full.tsv',
       file: './files/localization/compartments/Homo sapiens.tsv',
     },
     {
-      url: 'http://download.jensenlab.org/mouse_compartment_integrated_full.tsv',
+      url: 'https://download.jensenlab.org/mouse_compartment_integrated_full.tsv',
       file: './files/localization/compartments/Mus musculus.tsv',
     },
     {
-      url: 'http://download.jensenlab.org/yeast_compartment_integrated_full.tsv',
+      url: 'https://download.jensenlab.org/yeast_compartment_integrated_full.tsv',
       file: './files/localization/compartments/Saccharomyces cerevisiae.tsv',
     },
   ],
@@ -50,12 +49,12 @@ const localization = options => (
     ])
       .then(() => Promise.all([
         downloadHttps(fsConfig.hpa.url, fsConfig.hpa.zipFile, options.skipDownload),
-        downloadHttp(fsConfig.compartments[0].url, fsConfig.compartments[0].file, options.skipDownload),
-        downloadHttp(fsConfig.compartments[1].url, fsConfig.compartments[1].file, options.skipDownload),
-        downloadHttp(fsConfig.compartments[2].url, fsConfig.compartments[2].file, options.skipDownload),
-        downloadHttp(fsConfig.compartments[3].url, fsConfig.compartments[3].file, options.skipDownload),
-        downloadHttp(fsConfig.compartments[4].url, fsConfig.compartments[4].file, options.skipDownload),
-        downloadHttp(fsConfig.compartments[5].url, fsConfig.compartments[5].file, options.skipDownload),
+        downloadHttps(fsConfig.compartments[0].url, fsConfig.compartments[0].file, options.skipDownload),
+        downloadHttps(fsConfig.compartments[1].url, fsConfig.compartments[1].file, options.skipDownload),
+        downloadHttps(fsConfig.compartments[2].url, fsConfig.compartments[2].file, options.skipDownload),
+        downloadHttps(fsConfig.compartments[3].url, fsConfig.compartments[3].file, options.skipDownload),
+        downloadHttps(fsConfig.compartments[4].url, fsConfig.compartments[4].file, options.skipDownload),
+        downloadHttps(fsConfig.compartments[5].url, fsConfig.compartments[5].file, options.skipDownload),
       ]))
       .then(() => Promise.all([
         unzipFile(
